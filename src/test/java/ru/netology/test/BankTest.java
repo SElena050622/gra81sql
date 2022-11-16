ba33f1456d0ef540e1f0438e42b9f57570be54fb
Сем Елена <semova.s81@bk.ru>
2022-11-16 17:14:46 +0300

    made project

---
+++ src/test/java/ru/netology/test/BankTest.java
@@ -0,0 +1,32 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.*;
+import ru.netology.data.DataHelper;
+import ru.netology.data.SQLHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPage;
+import ru.netology.page.VerificationPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.data.SQLHelper.cleanDatabase;
+
+import org.junit.jupiter.api.Test;
+
+public class BankTest {
+
+    @AfterAll
+    static void teardown() {
+        cleanDatabase();
+    }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login password from sut text")
+    void shouldSuccessfullLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisiblity();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+}
